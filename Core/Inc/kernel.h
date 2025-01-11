e1d25ba371d256b45ac3ad8ff5272995909c9ce7
Mahir Mahota <mahirmahota@gmail.com>
2025-01-11 16:58:40 -0500

    adding local code

---
+++ Core/Inc/kernel.h
@@ -0,0 +1,42 @@
+/*
+ * kernel.h
+ *
+ *  Created on: Jun 24, 2024
+ *      Author: mahir
+ */
+
+#ifndef INC_KERNEL_H_
+#define INC_KERNEL_H_
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdio.h>
+#include "stm32f4xx_hal.h"
+#include <stdbool.h>
+
+#define STACK_SIZE 0x400
+#define TOTAL_STACK_SIZE 0x4000
+#define TOTAL_STACKS TOTAL_STACK_SIZE/STACK_SIZE
+#define DEFAULT_TIMEOUT_MS 5
+
+#define SHPR2 *(uint32_t*)0xE000ED1C //for setting SVC priority, bits 31-24
+#define SHPR3 *(uint32_t*)0xE000ED20 // PendSV is bits 23-16
+#define _ICSR *(uint32_t*)0xE000ED04 //This lets us trigger PendSV
+
+typedef struct k_thread {
+	uint32_t* sp; //stack pointer
+	void (*thread_function)(void*); //function pointer
+	uint32_t timeslice;
+	uint32_t runtime;
+} thread;
+
+void osKernelInitialize (void);
+uint32_t* osAllocateStack (void);
+bool osCreateThread (void (*thread_function)(void*), void* args);
+bool osCreateThreadWithDeadline (void (*thread_function)(void* args), void* args, uint32_t deadline);
+void osKernelStart (void);
+void osSched(void);
+void osYield(void);
+void SVC_Handler_Main (unsigned int *svc_args);
+
+#endif /* INC_KERNEL_H_ */
---
+++ Core/Src/kernel.c
@@ -0,0 +1,110 @@
+/*
+ * kernel.c
+ *
+ *  Created on: Jun 24, 2024
+ *      Author: mahir
+ */
+
+#include "kernel.h"
+
+extern void runFirstThread(void);
+
+uint32_t* prev_stack;
+uint32_t* msp_init_val;
+
+thread threads[TOTAL_STACKS];
+uint8_t curr_thread;
+uint8_t total_threads;
+
+void osKernelInitialize (void) {
+	msp_init_val = *(uint32_t**)0x0;
+	prev_stack = msp_init_val;
+	for (uint8_t i = 0; i < TOTAL_STACKS; i++) {
+		threads[i].sp = NULL;
+		threads[i].thread_function = NULL;
+		uint32_t timeslice = 0x0;
+		uint32_t runtime = timeslice;
+	}
+	curr_thread = 0;
+	total_threads = 0;
+
+	//set the priority of PendSV to almost the weakest
+	SHPR3 |= 0xFE << 16; //shift the constant 0xFE 16 bits to set PendSV priority
+	SHPR2 |= 0xFDU << 24; //Set the priority of SVC higher than PendSV
+}
+
+uint32_t* osAllocateStack(void) {
+	uint32_t* new_stack = prev_stack - STACK_SIZE;
+	if((uint32_t)msp_init_val - (uint32_t)new_stack > TOTAL_STACK_SIZE) {
+		return NULL;
+	}
+	prev_stack = new_stack;
+	return new_stack;
+}
+
+bool osCreateThread (void (*thread_function)(void* args), void* args) {
+	return osCreateThreadWithDeadline ((void*)thread_function, args, DEFAULT_TIMEOUT_MS);
+}
+
+bool osCreateThreadWithDeadline (void (*thread_function)(void* args), void* args, uint32_t deadline) {
+	uint32_t* stackptr = osAllocateStack();
+	if(stackptr == NULL) {
+		return false;
+	}
+
+	*(--stackptr) = 1 << 24; //xPSR
+	*(--stackptr) = (uint32_t)thread_function; //PC
+	for (uint8_t  i = 0; i < 14; i++) {
+		if(i == 5){ //R0
+			*(--stackptr) = args;
+		} else {
+			*(--stackptr) = 0xA;
+		}
+	}
+
+	threads[total_threads].sp = stackptr;
+	threads[total_threads].thread_function = thread_function;
+
+	threads[total_threads].timeslice = deadline;
+	threads[total_threads].runtime = deadline;
+	total_threads++;
+
+	return true;
+}
+
+void osKernelStart (void) {
+	__asm("SVC #1");
+}
+
+void osSched(void){
+	threads[curr_thread].runtime = threads[curr_thread].timeslice;
+	threads[curr_thread].sp = (uint32_t*)(__get_PSP() - 8*4);
+	curr_thread = (curr_thread + 1) % total_threads;
+	__set_PSP((uint32_t)threads[curr_thread].sp);
+}
+
+void osYield(void) {
+	__asm("SVC #2");
+}
+
+void SVC_Handler_Main(unsigned int *svc_args)
+{
+	unsigned int svc_number;
+	/*
+	* Stack contains:
+	* r0, r1, r2, r3, r12, r14, the return address and xPSR
+	* First argument (r0) is svc_args[0]
+	*/
+	svc_number = ( ( char * )svc_args[ 6 ] )[ -2 ] ;
+	switch( svc_number )
+	{
+		case 1:
+			__set_PSP((uint32_t)threads[curr_thread].sp);
+			runFirstThread();
+		case 2:
+			_ICSR |= 1 << 28;
+			__asm("isb");
+		default: /* unknown SVC */
+			break;
+	}
+}
